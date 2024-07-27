733514af756f14570e02492a989d4ae67f9ecd7e
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2024-07-26 21:15:14 -0300

    Test is added

---
+++ src/test/java/ar/com/laboratory/candidatesapi/CandidatesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.laboratory.candidatesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidatesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
