91ff700ef88fa66587eceb0561b1859efd9716a5
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2024-07-26 21:18:40 -0300

    Constants are added

---
+++ src/main/java/ar/com/laboratory/candidatesapi/infrastructure/util/Constants.java
@@ -0,0 +1,8 @@
+package ar.com.laboratory.candidatesapi.infrastructure.util;
+
+public class Constants {
+
+    public final static String CANDIDATE_TABLE = "candidates";
+
+    public final static String TAG_CANDIDATE_SERVICE = "CANDIDATES_SERVICE";
+}
