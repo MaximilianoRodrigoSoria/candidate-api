e5248b1f35e10f1934b0209c843784849c4e19cb
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2024-07-31 17:55:21 -0300

    Update postman collection

--- src/main/java/ar/com/laboratory/candidatesapi/infrastructure/controller/CandidateController.java
+++ src/main/java/ar/com/laboratory/candidatesapi/infrastructure/controller/CandidateController.java
@@ -5,6 +5,7 @@ import ar.com.laboratory.candidatesapi.domain.CandidateRequest;
 import ar.com.laboratory.candidatesapi.domain.CandidateResponse;
 import ar.com.laboratory.candidatesapi.domain.ErrorResponse;
 import ar.com.laboratory.candidatesapi.domain.ErrorsResponse;
+import ar.com.laboratory.candidatesapi.infrastructure.util.Constants;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -19,44 +20,44 @@ import java.util.List;
 @Tag(name="Candidate controller", description = "Obtain information on the candidate of the service", externalDocs = @ExternalDocumentation(description = "Obtain information on the health of the service", url = "http://localhost:8080/health"))
 public interface CandidateController {
 
-    @Operation(summary = "Create a new candidate")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Candidate deleted successfully",content = @Content(schema = @Schema(implementation = CandidateResponse.class))),
-            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = ErrorsResponse.class))),
-            @ApiResponse(responseCode = "404", description = "Candidate not found", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorsResponse.class)))
-    })
-    public ResponseEntity<CandidateResponse> createCandidate(CandidateRequest candidateRequest);
+@Operation(summary = "Create a new candidate")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Candidate created successfully", content = @Content(schema = @Schema(implementation = CandidateResponse.class, example = Constants.CANDIDATE_RESPONSE))),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = ErrorsResponse.class, example = Constants.ERROR_RESPONSE_400))),
+        @ApiResponse(responseCode = "404", description = "Candidate not found", content = @Content(schema = @Schema(implementation = ErrorResponse.class, example = Constants.ERROR_RESPONSE_404))),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponse.class, example = Constants.ERROR_RESPONSE_500)))
+})
+public ResponseEntity<CandidateResponse> createCandidate(CandidateRequest candidateRequest);
 
-    @Operation(summary = "Get a candidate by ID")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Candidate retrieved successfully", content = @Content(schema = @Schema(implementation = CandidateResponse.class))),
-            @ApiResponse(responseCode = "404", description = "Candidate not found", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
-    public ResponseEntity<CandidateResponse> getCandidate(Long id);
+@Operation(summary = "Get a candidate by ID")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Candidate retrieved successfully", content = @Content(schema = @Schema(implementation = CandidateResponse.class, example = Constants.CANDIDATE_RESPONSE))),
+        @ApiResponse(responseCode = "404", description = "Candidate not found", content = @Content(schema = @Schema(implementation = ErrorResponse.class, example = Constants.ERROR_RESPONSE_404))),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponse.class, example = Constants.ERROR_RESPONSE_500)))
+})
+public ResponseEntity<CandidateResponse> getCandidate(Long id);
 
-    @Operation(summary = "Update a candidate by ID")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Candidate deleted successfully",content = @Content(schema = @Schema(implementation = CandidateResponse.class))),
-            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = ErrorsResponse.class))),
-            @ApiResponse(responseCode = "404", description = "Candidate not found", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorsResponse.class)))
-    })
-    public ResponseEntity<CandidateResponse> updateCandidate(Long id, CandidateRequest candidateRequest);
+@Operation(summary = "Update a candidate by ID")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Candidate updated successfully", content = @Content(schema = @Schema(implementation = CandidateResponse.class, example = Constants.CANDIDATE_RESPONSE))),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = ErrorsResponse.class, example = Constants.ERROR_RESPONSE_400))),
+        @ApiResponse(responseCode = "404", description = "Candidate not found", content = @Content(schema = @Schema(implementation = ErrorResponse.class, example = Constants.ERROR_RESPONSE_404))),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponse.class, example = Constants.ERROR_RESPONSE_500)))
+})
+public ResponseEntity<CandidateResponse> updateCandidate(Long id, CandidateRequest candidateRequest);
 
-    @Operation(summary = "Delete a candidate by ID")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Candidate deleted successfully",content = @Content(schema = @Schema(implementation = CandidateResponse.class))),
-            @ApiResponse(responseCode = "404", description = "Candidate not found", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
-    public ResponseEntity<Void> deleteCandidate(Long id);
+@Operation(summary = "Delete a candidate by ID")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Candidate deleted successfully", content = @Content(schema = @Schema(implementation = CandidateResponse.class, example = Constants.CANDIDATE_RESPONSE))),
+        @ApiResponse(responseCode = "404", description = "Candidate not found", content = @Content(schema = @Schema(implementation = ErrorResponse.class, example = Constants.ERROR_RESPONSE_404))),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponse.class, example = Constants.ERROR_RESPONSE_500)))
+})
+public ResponseEntity<Void> deleteCandidate(Long id);
 
 @Operation(summary = "Get all candidates")
 @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Candidates retrieved successfully", content = @Content(schema = @Schema(implementation = CandidateResponse[].class))),
-        @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+        @ApiResponse(responseCode = "200", description = "Candidates retrieved successfully", content = @Content(schema = @Schema(implementation = CandidateResponse[].class, example = Constants.CANDIDATE_RESPONSE_ARRAY))),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponse.class, example = Constants.ERROR_RESPONSE_500)))
 })
 public ResponseEntity<List<CandidateResponse>> getCandidates();
 }
--- src/main/java/ar/com/laboratory/candidatesapi/infrastructure/util/Constants.java
+++ src/main/java/ar/com/laboratory/candidatesapi/infrastructure/util/Constants.java
@@ -5,4 +5,59 @@ public class Constants {
     public final static String CANDIDATE_TABLE = "candidates";
 
     public final static String TAG_CANDIDATE_SERVICE = "CANDIDATES_SERVICE";
+
+
+    public static final String CANDIDATE_RESPONSE_ARRAY = "[{\"id\":1,\"name\":\"John Doe\",\"email\":\"john.doe@example.com\",\"gender\":\"Male\",\"expectedSalary\":60000.00,\"active\":true,\"createdAt\":\"2023-01-01T00:00:00Z\",\"updatedAt\":\"2023-10-01T00:00:00Z\",\"typeOfContract\":\"Full-time\"}, {\"id\":2,\"name\":\"Jane Smith\",\"email\":\"jane.smith@example.com\",\"gender\":\"Female\",\"expectedSalary\":65000.00,\"active\":true,\"createdAt\":\"2023-02-01T00:00:00Z\",\"updatedAt\":\"2023-10-02T00:00:00Z\",\"typeOfContract\":\"Part-time\"}]";
+
+    public static final String CANDIDATE_RESPONSE = "{\"id\":1,\"name\":\"John Doe\",\"email\":\"john.doe@example.com\",\"gender\":\"Male\",\"expectedSalary\":60000.00,\"active\":true,\"createdAt\":\"2023-01-01T00:00:00Z\",\"updatedAt\":\"2023-10-01T00:00:00Z\",\"typeOfContract\":\"Full-time\"}";
+    public static final String ERROR_DESCRIPTION_500 = "Invalid request";
+
+    public static final String ERROR_DESCRIPTION_404 = "Not Found";
+
+    public static final String ERROR_DESCRIPTION_409 = "Conflict";
+
+    public static final String ERROR_DESCRIPTION_400 = "Bad Request";
+
+    public static final String ERROR_DESCRIPTION_401 = "Unauthorized";
+
+    public static final String ERROR_DESCRIPTION_403 = "Forbidden";
+
+    public static final String ERROR_CODE_500 = "500";
+
+    public static final String ERROR_CODE_404 = "404";
+
+    public static final String ERROR_CODE_409 = "409";
+
+    public static final String ERROR_CODE_400 = "400";
+    public static final String ERROR_RESPONSE_500 =  "{ \"status\": \"error\", \"code\": \"500\", \"message\": \"Internal Server Error\" }";
+
+    public static final String ERROR_RESPONSE_404 =  "{ \"status\": \"error\", \"code\": \"404\", \"message\": \"Not Found\" }";
+
+    public static final String ERROR_RESPONSE_409 =  "{ \"status\": \"error\", \"code\": \"409\", \"message\": \"Conflict\" }";
+
+    public static final String ERROR_RESPONSE_400 =  "{\n" +
+            "  \"classViolations\": [],\n" +
+            "  \"parameterViolations\": [\n" +
+            "    {\n" +
+            "      \"constraintType\": \"PARAMETER\",\n" +
+            "      \"message\": \"El nombre no puede estar vacío\",\n" +
+            "      \"path\": \"create.personaDTO.nombre\",\n" +
+            "      \"value\": \"\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"constraintType\": \"PARAMETER\",\n" +
+            "      \"message\": \"La edad no puede ser nula\",\n" +
+            "      \"path\": \"create.personaDTO.edad\",\n" +
+            "      \"value\": \"\"\n" +
+            "    }\n" +
+            "  ],\n" +
+            "  \"propertyViolations\": [],\n" +
+            "  \"returnValueViolations\": []\n" +
+            "}";
+
+    public static final String ERROR_RESPONSE_401 =  "{ \"status\": \"error\", \"code\": \"401\", \"message\": \"Unauthorized\" }";
+
+    public static final String ERROR_RESPONSE_403 =  "{ \"status\": \"error\", \"code\": \"403\", \"message\": \"Forbidden\" }";
+
+    public static final String APPLICATION_JSON =  "application/json";
 }
